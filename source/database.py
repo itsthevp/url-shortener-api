e74c54fa539d3193ecfff249213c5c080d3ad6f0
Vishv Patel <118260784+itsthevp@users.noreply.github.com>
2023-01-05 20:28:47 +0530

    fix: add email column in user table

--- source/database.py
+++ source/database.py
@@ -32,6 +32,7 @@ class UserModel(db.Model):
     id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
     first_name = db.Column(db.String(20), nullable=False)
     last_name = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(20), nullable=False)
     verified = db.Column(db.Boolean(), default=False)
